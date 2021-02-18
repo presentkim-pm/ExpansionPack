a2e7b003753ef4b8909ba1d61f77a343b3ba4a40
PresentKim <debe3721@gmail.com>
2021-02-19 01:52:22 +0900

    Register all creative items

--- src/kim/present/expansionpack/Loader.php
+++ src/kim/present/expansionpack/Loader.php
@@ -3,12 +3,26 @@ declare(strict_types=1);
 
 namespace kim\present\expansionpack;
 
+use pocketmine\inventory\CreativeInventory;
 use pocketmine\plugin\PluginBase;
 
 final class Loader extends PluginBase{
     protected function onLoad() : void{
+        //TODO: register blocks
+        //TODO: register items
+        //TODO: register block runtime ids
+        $this->registerAllCreativeItems();
     }
 
-    protected function onEnable() : void{
+    private function registerAllCreativeItems() : void{
+        $originItems = CreativeInventory::getInstance()->getAll();
+
+        CreativeInventory::reset();
+        $inv = CreativeInventory::getInstance();
+        foreach($originItems as $item){
+            if(!$inv->contains($item)){
+                $inv->add($item);
+            }
+        }
     }
 }
