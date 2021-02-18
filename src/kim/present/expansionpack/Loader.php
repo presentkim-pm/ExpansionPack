a612017861e0d3a9a8c6614cfef21637c3c0f1f9
PresentKim <debe3721@gmail.com>
2021-02-19 01:43:13 +0900

    Init ExpansionPack plugin

---
+++ src/kim/present/expansionpack/Loader.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+namespace kim\present\expansionpack;
+
+use pocketmine\plugin\PluginBase;
+
+final class Loader extends PluginBase{
+    protected function onLoad() : void{
+    }
+
+    protected function onEnable() : void{
+    }
+}
